11b407ac89c97d4c08f8e3e707a52b0f3078d036
Lykke Karlsson <kaly23qb@student.ju.se>
2024-04-05 17:50:51 +0200

    Put in starting point
    Added some functions & additional things to make the start productive

--- game.js
+++ game.js
@@ -2,12 +2,36 @@ let hedgehog;
 let bunny;
 let squirrel;
 
-let gridSize = 20;
-
 let rows = [];
 
 let gameIsRunning = true;
+let gridSize = 50;
+let results = 0;
+let level = 1;
+let gameMode = startScreen;
 
 function setup() {
   createCanvas();
+}
+
+function resetGame() {}
+
+function startScreen() {}
+function scenary() {}
+
+function levelOne() {}
+
+function levelTwo() {}
+
+function levelThree() {}
+
+function wonGame() {}
+
+function lostGame() {}
+
+function draw() {
+  if (gameMode === "startscreen") {
+    noStroke();
+    startScreen();
+  }
 }
