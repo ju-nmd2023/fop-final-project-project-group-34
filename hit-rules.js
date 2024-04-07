776a1e5006562437432d2bce0824e8df370773cb
osmo23jf <148855720+osmo23jf@users.noreply.github.com>
2024-04-07 21:06:54 +0200

    update hit-rules

--- hit-rules.js
+++ hit-rules.js
@@ -6,34 +6,22 @@ export default class hitRules {
     this.height = height;
   }
 
-  isColliding(otherhitRules) {
-    return (
-      this.x < otherhitRules.x + otherhitRules.width &&
-      this.x + this.width > otherhitRules.x &&
-      this.y < otherhitRules.y + otherhitRules.height &&
-      this.y + this.height > otherhitRules.y
-    );
-  }
-}
-
-
-
-overlaps (otherhitRules) {
+  overlaps(otherhitRules) {
     let left = this.x;
     let right = this.x + this.width;
     let top = this.y;
     let bottom = this.y + this.height;
-
 
     let otherLeft = otherhitRules.x;
     let otherRight = otherhitRules.x + otherhitRules.width;
     let otherTop = otherhitRules.y;
     let otherBottom = otherhitRules.y + otherhitRules.height;
 
-    return ! (
-        left >= otherRight ||
-        right <= otherLeft ||
-        top >= otherBottom ||
-        bottom <= otherTop
+    return !(
+      left >= otherRight ||
+      right <= otherLeft ||
+      top >= otherBottom ||
+      bottom <= otherTop
     );
+  }
 }
